bf753c984b5b7da5f3bbfc70634a1bbee2d771db
dgKim1 <kimdg3556@naver.com>
2023-07-16 23:41:49 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- management/server.js
+++ management/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,35 +7,26 @@ const port = process.env.PROT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers',(req,res) => {
-    res.send([
-      {
-        'id':1,
-        'image': 'https://picsum.photos/id/237/64/64',
-        'name': '홍길동',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '대학생'
+    connection.query(
+      "select * from customer",
+      (err, rows, fields) => {
+        res.send(rows);
       }
-      ,
-      {
-        'id':2,
-        'image': 'https://picsum.photos/id/236/64/64',
-        'name': '김도경',
-        'birthday': '000813',
-        'gender': '남자',
-        'job': '대학생'
-      }
-      ,
-      {
-        'id':3,
-        'image': 'https://picsum.photos/id/230/64/64',
-        'name': 'park',
-        'birthday': '001222',
-        'gender': '여자',
-        'job': '프로그래머'
-      }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
